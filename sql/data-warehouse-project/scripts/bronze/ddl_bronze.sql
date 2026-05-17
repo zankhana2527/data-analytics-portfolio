957535ec73bf492a34b1a61733ba8e2f02839846
Zankhana Panchal <75063692+zankhana2527@users.noreply.github.com>
2026-05-17 10:42:41 -0400

    Fix syntax error

--- sql/data-warehouse-project/scripts/bronze/ddl_bronze.sql
+++ sql/data-warehouse-project/scripts/bronze/ddl_bronze.sql
@@ -12,7 +12,7 @@ When to run:
 
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
-GO;
+GO
 
 CREATE TABLE bronze.crm_cust_info (
 	cst_id INT,
@@ -23,11 +23,11 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
-GO;
+GO
 
 IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
-GO;
+GO
 
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
@@ -42,7 +42,7 @@ GO;
 
 IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
-GO;
+GO
 
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
@@ -55,32 +55,32 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_quantity INT,
 	sls_price INT
 );
-GO;
+GO
 
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
-GO;
+GO
 
 CREATE TABLE bronze.erp_cust_az12 (
 	cid NVARCHAR(50),
 	bdate DATE,
 	gen NVARCHAR(50)
 );
-GO;
+GO
 
 IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
-GO;
+GO
 
 CREATE TABLE bronze.erp_loc_a101 (
 	cid NVARCHAR(50),
 	cntry NVARCHAR(50)
 );
-GO;
+GO
 
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
-GO;
+GO
 
 CREATE TABLE bronze.erp_px_cat_g1v2 (
 	id NVARCHAR(50),
@@ -88,4 +88,4 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
 	subcat NVARCHAR(50),
 	maintenance NVARCHAR(50)
 );
-GO;
+GO

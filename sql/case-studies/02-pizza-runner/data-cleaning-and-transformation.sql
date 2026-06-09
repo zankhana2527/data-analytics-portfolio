bbe4c96b0d91d2729e79aa4823832a2d968a091e
Zankhana Panchal <75063692+zankhana2527@users.noreply.github.com>
2026-06-09 19:33:38 -0400

    Add data cleaning and transformation SQL script
    This script cleans and transforms data from customer_orders and runner_orders tables, creating temporary tables for processed data.

---
+++ sql/case-studies/02-pizza-runner/data-cleaning-and-transformation.sql
@@ -0,0 +1,43 @@
+-- Clean and transform the data
+-- Create temp table in sql server by assigning # sign in front of the new table name
+
+==============================================
+-- CLEAN AND TRANSFORM customer_orders TABLE 
+==============================================
+DROP TABLE IF EXISTS #temp_customer_orders;
+SELECT
+    order_id,
+    customer_id,
+    pizza_id,
+    CASE WHEN exclusions = '' OR exclusions = 'null' THEN NULL
+         ELSE exclusions
+    END AS exclusions,
+    CASE WHEN extras = '' OR extras = 'null' THEN NULL
+         ELSE extras
+    END AS extras,
+    order_time
+INTO #temp_customer_orders
+FROM customer_orders;
+
+==============================================
+-- CLEAN AND TRANSFORM runner_orders TABLE 
+==============================================
+
+DROP TABLE IF EXISTS #temp_runner_orders;
+SELECT
+    order_id,
+    runner_id,
+    CASE WHEN pickup_time LIKE 'null' THEN NULL
+         ELSE pickup_time
+    END AS pickup_time,
+    CASE WHEN distance LIKE 'null' THEN NULL
+         WHEN distance LIKE '%km' THEN TRIM ('km' FROM distance)
+    END AS distance,
+    CASE WHEN duration LIKE '%min%' THEN TRIM(TRAILING 'minutes ' FROM duration)
+         WHEN duration LIKE 'null' THEN NULL
+    END AS duration,
+    CASE WHEN cancellation LIKE 'null' OR cancellation = '' THEN NULL
+         ELSE cancellation
+END AS cancellation
+INTO #temp_runner_orders
+FROM runner_orders;
